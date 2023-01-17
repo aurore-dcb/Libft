c208fa578a76e6e2f5a5aa1bf06df9669c3bf33b
Aurore Ducobu <aurore@air-de-aurore.home>
2023-01-17 15:03:01 +0100

    modif strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -15,25 +15,18 @@ static int in_set(char const *set, const char c)
 }
 char *ft_strtrim(char const *s1, char const *set)
 {
-    size_t i;
-    size_t indice;
-    char *res;
+    char	*ret;
+	char	*start;
+	char	*end;
 
-    if (!s1)
-        return (ft_strdup(""));
-    i = 0;
-    indice = 0;
-    while (s1[i] && in_set(set, s1[i]) == 1)
-    {
-        indice++;
-        i++;
-    }
-    i = ft_strlen(s1) - 1;
-    while (i > indice && in_set(set, s1[i]) == 1)
-        i--;
-    res = malloc(sizeof(char) * i - indice + 1);
-    if (!res)
-        return (NULL);
-    res = ft_substr(s1, (unsigned int)indice, i - indice + 1);
-    return (res);
-}
+	if (!s1 || !set)
+		return (0);
+	start = (char *)s1;
+	end = start + ft_strlen(s1);
+	while (*start && in_set(set, *start))
+		++start;
+	while (start < end && in_set(set, *(end - 1)))
+		--end;
+	ret = ft_substr(start, 0, end - start);
+	return (ret);
+}g
